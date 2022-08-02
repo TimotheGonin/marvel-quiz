b6adf321e90846e6f31cfbb6cc8352b48357b96a
TimotheGonin <gonin.timothe@gmail.com>
2022-08-02 15:18:55 +0200

    🚧 📦 add react router dom + routes and components

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,17 +1,29 @@
-import Header from '../Header';
-import Landing from '../Landing';
-import Footer from '../Footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import '../../App.css';
+import Header from '../Header'
+import Landing from '../Landing'
+import Footer from '../Footer'
+import Welcome from '../Welcome'
+import Login from '../Login'
+import SignUp from '../SignUp'
+import ErrorPage from '../ErrorPage'
+
+import '../../App.css'
 
 function App() {
   return (
-    <div>
-      <Header/>
-      <Landing/>
-      <Footer/>
-    </div>
-  );
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/welcome" element={<Welcome />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
