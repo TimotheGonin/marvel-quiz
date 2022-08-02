142d4fd7d42d9c43c3c57b9b094cf70d69d2eb9e
TimotheGonin <gonin.timothe@gmail.com>
2022-08-02 11:27:58 +0200

    ✨ feat: display buttons after claws

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const Landing = () => {
+  const refWolverine = useRef(null)
+  const [button, setButton] = useState(false)
+
+  useEffect(() => {
+    refWolverine.current.classList.add('startingImg')
+    setTimeout(() => {
+      refWolverine.current.classList.remove('startingImg')
+      setButton(true)
+    }, 1000)
+  }, [])
+
+  const displayButton = button && (
+    <React.Fragment>
+      <div className="leftBox">
+        <button className="btn-welcome">Inscription</button>
+      </div>
+      <div className="rightBox">
+        <button className="btn-welcome">Connexion</button>
+      </div>
+    </React.Fragment>
+  )
+
   return (
-    <main className="welcomePage"></main>
+    <main ref={refWolverine} className="welcomePage">
+      {displayButton}
+    </main>
   )
 }
 
